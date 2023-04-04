7cf5519a863c73ca7651ad94ba1234f69ed3fe99
miller1995 <nircaanton95@gmail.com>
2023-04-04 20:17:40 +0300

    - added new java class Teacher, in this class created fields ( firstName, lastName etc.); - created null arguments constructor and constructor with full arguments; - created getters and setters;

---
+++ src/main/java/com/startup/models/Teacher.java
@@ -0,0 +1,91 @@
+package com.startup.models;
+
+public class Teacher {
+
+    private long teacherId;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private int cnp;
+    private String address;
+    private String phoneNumber;
+    private String email;
+
+    public Teacher(){}
+
+    public Teacher(long teacherId, String firstName, String lastName, String gender,
+                   int cnp, String address, String phoneNumber, String email){
+        this.teacherId = teacherId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.cnp = cnp;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public long getTeacherId(){
+        return teacherId;
+    }
+
+    public void setTeacherId(long teacherId){
+        this.teacherId = teacherId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getCnp() {
+        return cnp;
+    }
+
+    public void setCnp(int cnp) {
+        this.cnp = cnp;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

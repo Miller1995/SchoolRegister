ce3225b0e7053a1c8cceaafbfc767cc7f19fbdd3
Iulian <iulian.ionut.buga@gmail.com>
2023-04-04 11:40:30 +0300

    -added a new view controller to the Controller class

--- src/main/java/com/startup/controllers/Controller.java
+++ src/main/java/com/startup/controllers/Controller.java
@@ -5,7 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.stereotype.Controller
 public class Controller {
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/testController")
+    public String testController() {
+        return "testController";
     }
 }
